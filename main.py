7becddb4b5e8add28c7a249e00322786e5106992
Gabriel Witor <witor.alm@gmail.com>
2023-10-29 11:32:09 -0300

    finished CRUD

--- main.py
+++ main.py
@@ -5,22 +5,13 @@ dbname = get_database()
 collection_name = dbname["aluno"]
 
 
-# item_3 = {
-#     "_id" : 3,
-#     "nome": "Gustav Keller",
-#     "idade": 19,
-#     "email": "gustav.keller@gmail.com",
-#     "senha" : "maskeiko",
-# }
-# collection_name.insert_one(item_3)
-
 while(1):
     print("1 - Inserir\n2 - Ler\n3 - Atualizar\n4 - Deletar\n5 - Encerrar")
-    option = 1
+    option = 0
     
     try:
         option = int(input("Digite a opção: "))
-        if 0 > option or option > 4:
+        if 0 > option or option > 5:
             raise OverflowError
     except (ValueError, TypeError, OverflowError):
         print("Essa não é uma opção válida")
@@ -35,18 +26,55 @@ while(1):
                 if greatest_id < item['_id']:
                     greatest_id = item['_id']
                     
-            print(greatest_id)
-
+            name = input("Digite o nome do aluno: ")
+            age = int(input("Digite a idade do aluno: "))
+            email = input("Digite o email do aluno: ")
+            password = input("Digite a senha do aluno: ")
             
-        
+            item = {
+                "_id" : greatest_id+1,
+                "nome": name,
+                "idade": age,
+                "email": email,
+                "senha" : password,
+            }
+            collection_name.insert_one(item)
 
-        # case 2:
+        case 2:
+            item_details = collection_name.find()
+            for item in item_details:
+                print(f"Id: {item['_id']} | Nome: {item['nome']} | Idade: {item['idade']} | Email: {item['email']}\n")
         
-
-        # case 3:
+        case 3:
+            
+            try:
+                id_to_update = int(input("Digite o id do aluno que deseja modificar: "))
+            except (ValueError, TypeError):
+                print("Essa não é uma opção válida")
+                
+            name = input("Digite o nome do aluno: ")
+            age = int(input("Digite a idade do aluno: "))
+            email = input("Digite o email do aluno: ")
+            password = input("Digite a senha do aluno: ")
+            
+            filter = filter = { '_id': id_to_update }
+            
+            item = { "$set": {
+                "nome": name,
+                "idade": age,
+                "email": email,
+                "senha" : password,
+            } } 
+            collection_name.update_one(filter, item)
+            
+        case 4:
+            try:
+                id_to_delete = int(input("Digite o id do aluno que deseja deletar: "))
+            except (ValueError, TypeError):
+                print("Essa não é uma opção válida")
+                
+            collection_name.delete_one( { "_id": id_to_delete } )
         
-    
-        # case 4:
-        
-        # case _:
+        case 5:
+            break
 
